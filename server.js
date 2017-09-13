bbbc2c59f5df2cd2b0cea4d52c733391c19d336f
Tanapon Meesat <tanapon.me@ku.th>
2017-09-14 00:28:26 +0700

    debug2

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ var path = require('path');
 var serveStatic = require('serve-static');
 app = express();
 app.use(serveStatic(__dirname));
-var port = process.env.PORT || 8021;
+var port = process.env.PORT || 8022;
 app.listen(port);
 console.log('server started '+ port);
